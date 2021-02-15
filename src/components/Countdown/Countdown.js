a8883989390f3cb97a77893a76c11d99a68bd4bd
Cooper Saye <coopersaye@gmail.com>
2021-02-15 12:49:18 -0700

    Improve Countdown component time display.

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -1,11 +1,24 @@
+import './Countdown.css';
+
 import React, { useEffect, useState } from 'react';
+
+import firebase from 'firebase/app';
+
+const SEC_MS = 1000;
+const MIN_MS = SEC_MS * 60;
+const HOUR_MS = MIN_MS * 60;
+const DAY_MS = HOUR_MS * 24;
 
 function Countdown(props) {
   // get title and end time from data
-  const { title, endDateTime } = props.data;
+  const { title, id, endDateTime } = props.data;
   const endDate = endDateTime.toDate();
 
   const [timeLeft, setTimeLeft] = useState(endDate - new Date());
+
+  async function deleteCountdown() {
+    await firebase.firestore().collection('countdowns').doc(id).delete();
+  }
 
   useEffect(() => {
     // update time left every second
@@ -18,8 +31,14 @@ function Countdown(props) {
   return (
     <div className="Countdown">
       <h1>{title}</h1>
-      <p>{endDate.toString()}</p>
-      <p>{timeLeft / 1000} seconds left</p>
+      <p>{endDate.toDateString() + ', ' + endDate.toLocaleTimeString()}</p>
+      <p>
+        <span className="time-num">{Math.floor(timeLeft / DAY_MS)}</span>d
+        <span className="time-num">{Math.floor((timeLeft % DAY_MS) / HOUR_MS)}</span>h
+        <span className="time-num">{Math.floor(((timeLeft % DAY_MS) % HOUR_MS) / MIN_MS)}</span>m
+        <span className="time-num">{Math.floor((((timeLeft % DAY_MS) % HOUR_MS) % MIN_MS) / SEC_MS)}</span>s
+      </p>
+      <button onClick={deleteCountdown}>Delete</button>
     </div>
   );
 }
--- src/components/MakeCountdown/MakeCountdown.js
+++ src/components/MakeCountdown/MakeCountdown.js
@@ -4,19 +4,24 @@ import firebase from 'firebase/app';
 
 function MakeCountdown() {
   const [title, setTitle] = useState('');
-  const [endTime, setEndTime] = useState('');
   const [endDate, setEndDate] = useState('');
+  const [endTime, setEndTime] = useState('');
 
   async function addCountdown(e) {
     e.preventDefault();
     const endDateTime = new Date(endDate + ' ' + endTime);
     const uid = firebase.auth().currentUser.uid;
     const countdownsRef = firebase.firestore().collection('countdowns');
+    // add countdown
     await countdownsRef.add({
       endDateTime,
       title,
       uid
     });
+    // reset inputs
+    setTitle('');
+    setEndDate('');
+    setEndTime('');
   }
 
   return (
@@ -37,6 +42,7 @@ function MakeCountdown() {
         />
         <input
         type="time"
+        step="1"
         value={endTime}
         onChange={e => setEndTime(e.target.value)}
         required
