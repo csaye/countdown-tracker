2bf68c8204c0abc379d78bf227c9bd88b967ae31
Cooper Saye <coopersaye@gmail.com>
2021-02-15 09:16:13 -0700

    Implement sign in and sign out.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,18 @@
+import firebase from 'firebase/app';
+
 function Header() {
+  // signs out current user
+  function signOut() {
+    firebase.auth().signOut();
+  }
+
   return (
     <div className="Header">
-      <p>Header</p>
+      <h1>Countdown Tracker</h1>
+      {
+        firebase.auth().currentUser &&
+        <button onClick={signOut}>Sign Out</button>
+      }
     </div>
   )
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,7 +1,15 @@
+import firebase from 'firebase/app';
+
 function SignIn() {
+  // opens google sign in popup
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   return (
     <div className="SignIn">
-      <p>SignIn</p>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   )
 }
