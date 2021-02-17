d8748c6d3d9e433d551b77d97a068bdd4d5994a5
Cooper Saye <coopersaye@gmail.com>
2021-02-16 19:44:23 -0700

    Implement NewCountdown opening and closing.

--- src/components/NewCountdown/NewCountdown.js
+++ src/components/NewCountdown/NewCountdown.js
@@ -5,12 +5,16 @@ import React, { useState } from 'react';
 import firebase from 'firebase/app';
 
 function NewCountdown() {
+  const [open, setOpen] = useState(false);
+
   const [title, setTitle] = useState('');
   const [endDate, setEndDate] = useState('');
   const [endTime, setEndTime] = useState('');
 
   async function addCountdown(e) {
     e.preventDefault();
+    // close
+    setOpen(false);
     const endDateTime = new Date(endDate + ' ' + endTime);
     const uid = firebase.auth().currentUser.uid;
     const countdownsRef = firebase.firestore().collection('countdowns');
@@ -28,29 +32,38 @@ function NewCountdown() {
 
   return (
     <div className="NewCountdown card">
-      <p>New Countdown</p>
-      <form onSubmit={addCountdown}>
-        <input
-        type="text"
-        value={title}
-        onChange={e => setTitle(e.target.value)}
-        required
-        />
-        <input
-        type="date"
-        value={endDate}
-        onChange={e => setEndDate(e.target.value)}
-        required
-        />
-        <input
-        type="time"
-        step="1"
-        value={endTime}
-        onChange={e => setEndTime(e.target.value)}
-        required
-        />
-        <button type="submit">Create</button>
-      </form>
+      {
+        open ?
+        <div>
+          <button onClick={() => setOpen(false)}>✖</button>
+          <p>New Countdown</p>
+          <form onSubmit={addCountdown}>
+            <input
+            type="text"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            required
+            />
+            <input
+            type="date"
+            value={endDate}
+            onChange={e => setEndDate(e.target.value)}
+            required
+            />
+            <input
+            type="time"
+            step="1"
+            value={endTime}
+            onChange={e => setEndTime(e.target.value)}
+            required
+            />
+            <button type="submit">Create</button>
+          </form>
+        </div> :
+        <div className="center-container">
+          <button className="plus-button" onClick={() => setOpen(true)}>+</button>
+        </div>
+      }
     </div>
   )
 }
